5337f30b5ca46b82cd28cfde463fcabc5918c3da
Aditya Agrawal <adityaag1407@gmail.com>
2025-08-07 12:34:55 +0530

    Make some changes

--- matmulpy.py
+++ matmulpy.py
@@ -17,12 +17,19 @@ M=np.array([
     [0,0,0],
     [0,0,0]
 ])
+inv=np.array([
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+])
+print("THE PRODUCT OF A & B IS: ")
 for i in range(3):
     for j in range(3):
          for k in range(3):
              M[i][j]+=A[i][k]*B[k][j]
          print (M[i][j],"",end=" ")
     print("\n")    
+print("THE DETERMINANT OF B IS: ")
 
 
 
